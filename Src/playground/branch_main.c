f803a1c7cce948291ab64f47d3baa745f6940ca0
Eric Xu <xydxydxyd1@gmail.com>
2024-10-13 21:48:39 -0700

    Draft exercise (not buildable due to leda)

--- Src/playground/branch_main.c
+++ Src/playground/branch_main.c
@@ -4,9 +4,32 @@
 #include <Buttons/buttons.h>
 #include <print_scan.h>
 
+void main_loop() {
+	// Button reaciton
+	if (get_buttonStatus_1()) {
+		printMsg("Button 1 is pressed");
+	}
+	if (get_buttonStatus_0()) {
+		printMsg("Button 0 is pressed");
+	}
+
+	// Blink
+	static int last_blink_time = -1000;
+	static bool last_blink_state = false;
+	int currtime = getSysTime();
+	if (currtime - last_blink_time >= 1000) {
+		last_blink_time = currtime;
+		last_blink_state = !last_blink_state;
+		led_a(last_blink_state);
+	}
+}
+
 void branch_main() {
 	// space for initializations
 
+	while (1) {
+		main_loop();
+	}
 
 	// infinite loop
 	while (1) {
